15b4dbdfbf794225674f3a57325af25a182b426e
Jessele V Del Mundo <ashtridgray@gmail.com>
2019-03-30 17:25:18 +0800

    Create Modals and Update Functions (Edit & Delete)
    -Create Edit Modals
    -Fix Edit function
    -Create Delete Modal
    -Fix Delete functions

--- gmusic/angularjs/controllers/mainCtrl.js
+++ gmusic/angularjs/controllers/mainCtrl.js
@@ -1,60 +1,43 @@
-GMusic.controller('mainCtrl', function($scope, $filter, $firebaseArray){
-  // alert("test");  
-  // firebase.initializeApp(config);
-  // const rootRef = firebase.database().ref().child('firemusicdb');
-  // this.playlists = $firebaseObject(ref);
-  // const ref = rootRef.child('playlists');
-  // this.playlists = $firebaseObject(ref);
+GMusic.controller('mainCtrl', function($scope, $http, $firebaseArray, $firebaseObject){
+    // alert("mainCtrl");
+    //Firebase Functions
 
-  // $scope.currentPage = 0;
-  // $scope.pageSize = 10;
-  // $scope.data = [];
-  $scope.q = '';
-  // $scope.getData = function () {
-  //   return $filter('filter')($scope.playlists, $scope.q)
-  // }
-  // $scope.numberOfPages=function(){
-  //   return Math.ceil($scope.getData().length/$scope.pageSize);
-  // }
-
-  // for (var i=0; i<65; i++) {
-  //     $scope.data.push("Item "+i);
-  // }
-
-  // $scope.$watch('q', function(newValue,oldValue){                       
-  //   if(oldValue!=newValue){
-  //     $scope.currentPage = 0;
-  //   }
-  //   },true);
-  // }]);
-
-  //Firebase Functions
-  var ref = firebase.database().ref();
-
-  $scope.playlists = $firebaseArray(ref);
-  $scope.q='';
-  $scope.add=function () {
-      $scope.playlists.$add($scope.gmusic);
-      $scope.gmusic = "";
-  }
-  $scope.edit=function () {
-    // body...
-    // $scope.playlists.$update($scope.gmusic);
-  }
-
-  $scope.clearForm = function(gmusic){
+    // add
+    var ref = firebase.database().ref().child('Songs');
+    $scope.playlists = $firebaseArray(ref);
+    console.log($scope.playlists);
+    $scope.add = function() {
+        $scope.playlists.$add($scope.gmusic);
         $scope.gmusic = "";
-  }   
-  // $scope.playlists="brook - binkusu sake";
+    }
+    // edit
+    $scope.edit = function(id){
+        var ref = firebase.database().ref().child('Songs/' + id);
+        $scope.editdata = $firebaseObject(ref);
+        console.log($scope.editdata);
 
-
-  //Filter
-  // GMusic.filter('startFrom', function() {
-  //   return function(input, start) {
-  //       start = +start;
-  //       return input.slice(start);
-  //   }
-  // });
-
+    }
+    // save what you edit
+    $scope.save = function(id){
+        var ref = firebase.database().ref().child('Songs/' + id);
+        ref.update({
+            song_title: $scope.editdata.song_title,
+            song_artist: $scope.editdata.song_artist,
+            song_size: $scope.editdata.song_size
+        }).then(function(ref){
+            $('#editModal').modal('hide');
+        }, function(err){
+            console.log(err);
+        });
+    };
+    // delete modal get the ID to delete
+    $scope.deleteData = function(item){
+        $scope.deleteItem = item;
+        console.log(item);
+    }
+    $scope.delete = function(deleteItem){
+        $scope.playlists.$remove(deleteItem);
+        $('#deleteModal').modal('hide');
+    }
 
 });
